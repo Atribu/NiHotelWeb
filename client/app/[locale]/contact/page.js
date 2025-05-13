2f70c80ec5819544f56bac7f190cd33d87b7764c
Ece Bayramer <ecebayramer20@gmail.com>
2025-05-13 16:48:03 +0300

    gallery added

--- client/app/[locale]/contact/page.js
+++ client/app/[locale]/contact/page.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ContactBanner from "./components/ContactBanner"
 import ContactSection from './components/ContactSection'
+import img from "../../../public/images/lobi/DSCF8651.webp"
+import { useTranslations } from 'next-intl';
 
 const page = () => {
+  const t = useTranslations('Contact');
+  
   return (
     <div>
-      <ContactBanner/>
+      <ContactBanner image={img} header={t("header")}/>
       <ContactSection/>
     </div>
   )
--- client/app/[locale]/gallery/page.js
+++ client/app/[locale]/gallery/page.js
@@ -7,6 +7,14 @@ import img4 from "../../../public/images/lobi/DSCF8673.webp"
 import img5 from "../../../public/images/lobi/DSCF8675.webp"
 import img6 from "../../../public/images/lobi/DSCF8676.webp"
 import img7 from "../../../public/images/lobi/DSCF8677.webp"
+import img8 from "../../../public/images/lobi/DSCF8651yakın.webp"
+import img9 from "../../../public/images/lobi/DSCF8656.webp"
+import img10 from "../../../public/images/lobi/DSCF8661.webp"
+import img11 from "../../../public/images/lobi/DSCF8695.webp"
+import img12 from "../../../public/images/lobi/DSCF8700.webp"
+import img13 from "../../../public/images/lobi/DSCF8702.webp"
+import img14 from "../../../public/images/lobi/DSCF8704.webp"
+import ContactBanner from "../contact/components/ContactBanner"
 
 export const metadata = {
   title: "Galeri – NiHotel",
@@ -21,16 +29,15 @@ const images = [
     img4,
     img5,
   img6,
-  img7
+  img7,
+  img8,img9,img10,img11,img12,img13,img14
 ];
 
 export default function GalleryPage() {
   return (
     <main className="bg-gray-50 min-h-screen">
+      <ContactBanner header="Galeri" image={img2}/>
       <section className="container mx-auto px-4 py-12">
-        <h1 className="text-3xl md:text-4xl font-semibold text-gray-800 text-center mb-8">
-          Galeri
-        </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {images.map((src, idx) => (
             <div key={idx} className="overflow-hidden shadow-lg group">
