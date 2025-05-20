710dc33ca9aa39ea49cb9f4b29a40648adaf1883
Ece Bayramer <ecebayramer20@gmail.com>
2025-05-20 15:04:48 +0300

    video edited

--- client/app/[locale]/components/Homepage/MainBanner.jsx
+++ client/app/[locale]/components/Homepage/MainBanner.jsx
@@ -14,7 +14,7 @@ export default function HomePage() {
       playsInline
       className=" top-0 left-0 w-full h-full object-cover hidden lg:flex relative">
       <source 
-        src="/videos/nihotelsondesktop.mp4"
+        src="/videos/newDesktop.mp4"
         type="video/mp4"
       />
       Tarayıcınız bu videoyu desteklemiyor.
@@ -29,7 +29,7 @@ export default function HomePage() {
       className="absolute top-0 left-0 w-full h-full object-cover flex lg:hidden"
     >
       <source 
-        src="/videos/nihotelsonmobile.mp4"
+        src="/videos/newMobile.mp4"
         type="video/mp4"
       />
       Tarayıcınız bu videoyu desteklemiyor.
