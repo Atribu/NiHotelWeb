f177cc225cf5a23c4e7f2c8c081777a0d407382a
Ece Bayramer <ecebayramer20@gmail.com>
2025-05-22 15:28:32 +0300

    edited

--- client/app/[locale]/rooms/verandaroom/page.js
+++ client/app/[locale]/rooms/verandaroom/page.js
@@ -44,7 +44,7 @@ const Page = () => {
     />
     <ReservationSection/>
     <RoomFeatures/>
-    <RoomTour link="https://kuula.co/share/n1/collection/71qXs"/>
+    <RoomTour link="https://kuula.co/share/n1/collection/71XBq"/>
     <DiscoverRooms header={t2("header")} buttonText={t2("buttonText")} name1={t2("name1")} description1={t2("description1")} name2={t2("name2")} description2={t2("description2")} name3={t2("name3")} description3={t2("description3")} link1="/rooms/standardroom" link2="/rooms/juniorroom" link3="/rooms/cornerroom"/>
     <Image src={img} alt='portre' width={800} height={1400} className='absolute top-[1200px] left-0 z-[1]  '/>
     </div>
